de446343681f1b16e9e30a62e18c59c079dc8926
Clément Chardin <chardinclement@gmail.com>
2022-03-30 11:13:04 +0200

    Update to SiC scripts
    created a file with numerical values and updated the main script.

---
+++ SiC/numerical_values.py
@@ -0,0 +1,28 @@
+# -*- coding: cp1252 -*-
+"""
+Numerical values for the SiC nanowires we measured
+
+Author :
+    Clément Chardin
+    clement.chardin@nottingham.ac.uk
+"""
+
+# Length of the NW, in µm
+lengths = {'Fil 1':405,
+           'Fil 2':275,
+           'Fil 3':346,
+           'Fil 4':340,
+           'Fil 8':327,
+           }
+
+# Diameter of the NW, in nm
+diameters = {'Fil 1':267,
+             'Fil 2':450,
+             'Fil 3':323,
+             'Fil 4':550,
+             }
+
+# Step of the manual measurement, in µm
+steps = {'Fil 2':20,
+         'Fil 4':20,
+         }
--- SiC/script_SiC.py
+++ SiC/script_SiC.py
@@ -12,11 +12,13 @@ from matplotlib.pyplot import *
 from scipy.optimize import least_squares
 import os
 from donnees_rsa import *
+from numerical_values import *
+
+fil = '4'
+savefigs = True
 
 dat = '20211126'
-batch = 'Fil 2 manuel'
-
-savefigs = True
+batch = 'Fil %s manuel'%fil
 
 df = 500 # Margin left and right of the maximum of the spectrum for peak detection (in Hz)
 generalWidth = 5 # Guess for Gamma/2pi (in Hz)
@@ -27,6 +29,7 @@ if savefigs and not os.path.isdir(figsdir):
     os.mkdir(figsdir)
     print('Figures directory created :', figsdir)
 
+""" Define functions """
 def lorentzian(omega, omega0, gamma, a):
     return a * omega0**2 * gamma**2 / ((omega**2 - omega0**2)**2 + omega**2*gamma**2)
 
@@ -40,6 +43,13 @@ def res_multi_lorentz(params, xData, yData):
     diff = [multi_lorentz(x, params) - y for x, y in zip(xData, yData)]
     return diff
 
+""" Load numerical values """
+key = 'Fil %s'%fil
+length = lengths[key]
+diameter = diameters[key]
+step = steps[key]
+
+""" Do the fits """
 popts = []
 positions = []
 _, _, filenames = next(os.walk(dirname), (None, None, [])) # Get all files in dirname, excluding sub-directories
@@ -48,7 +58,8 @@ for ii, filename in enumerate(sorted(filenames)):
     print(filename)
 
     position = int(filename.split('.')[0].split(' ')[1])
-    positions.append(position)
+    position_norm = 1 - (position-1)*step/length
+    positions.append(position_norm)
 
     freqs, spectrum = get_freqs_data(filename, Trace=1, unit='Hz', dirname=dirname)
     omegas = freqs * 2 * pi
@@ -90,7 +101,7 @@ for ii, filename in enumerate(sorted(filenames)):
         errors.append(ii)
         break
 
-    # Plot the fit
+    """ Plot the fit """
     fit = array([multi_lorentz(2*pi*f, popt) for f in freqs])
     offset, omega0, gamma, ampl = popt
 
@@ -110,21 +121,22 @@ for ii, filename in enumerate(sorted(filenames)):
         ax.set_xlim(x1, x2)
         print('Figure saved,', filename.split('.')[0])
 
+""" Plot figures """
 fig_freqs, ax_freqs = subplots()
 plot(positions, [popt[1]/2/pi for popt in popts], 'o')
-xlabel('Position (arb.)')
+xlabel('Normalized position')
 ylabel('Frequency (Hz)')
 title('Frequency')
 
 fig_gamma, ax_gamma = subplots()
 plot(positions, [popt[2]/2/pi for popt in popts], 'o')
-xlabel('Position (arb.)')
+xlabel('Normalized position')
 ylabel('$\Gamma/2\pi$ (Hz)')
 title('Damping rate')
 
 fig_ampl, ax_ampl = subplots()
 plot(positions, [popt[3] for popt in popts], 'o')
-xlabel('Position (arb.)')
+xlabel('Normalized position')
 ylabel('Amplitude (arb.)')
 title('Peak amplitude')
 
